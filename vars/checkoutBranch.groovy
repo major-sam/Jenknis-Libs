4e5ca5f0f8c5e179795c99acc3c911558cdadf70
vrebyachih <vrebyachih@baltbet.ru>
2021-08-30 12:34:54 +0300

    add checkout lib

---
+++ vars/checkoutBranch.groovy
@@ -0,0 +1,14 @@
+def call(Map config = [:]){
+  echo ("Это костыльный шаг - вложенный чекаут для выбранной ранее ветки.")
+  echo "You choose ${config.BRANCH} branch, checkout"
+  BRANCH = config.BRANCH.trim().toString().replaceAll("\\s","")
+  env.DEFAULT_BRANCH = env.DEFAULT_BRANCH.trim().toString().replaceAll("\\s","")
+  withCredentials([gitUsernamePassword(credentialsId: config.creds)]) {
+  	if (BRANCH.contains (env.DEFAULT_BRANCH)) {
+    	  powershell ( encoding:"UTF8", script: "git clone ${config.GIT_URL} ${config.CLONE_FOLDER}")
+  	}
+  	else {
+  	  powershell ( encoding:"UTF8", script: "git clone --single-branch --branch ${config.BRANCH} ${config.GIT_URL} ${config.CLONE_FOLDER}")
+  	}
+  }
+}
