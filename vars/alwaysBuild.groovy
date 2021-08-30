1d612d07432bc2c1e3494077684fa9badd4a03c4
vrebyachih <vrebyachih@baltbet.ru>
2021-08-30 19:12:41 +0300

    add always lib

---
+++ vars/alwaysBuild.groovy
@@ -0,0 +1,12 @@
+def call(Map config = [:]){          
+        echo "Build must be manualy enabled in Jira>Manage Apps>Jenkins Integration>Manage Sites>Jenkins>Search Job>${config.job} "
+        step([$class: 'DeploymentBuildMarker', environmentType: 'production' , environmentName: 'testing'])
+        if (!((config.branch ==~ env.BRANCH_REGEX )||(config.branch in config.default_branches))){
+          catchError(message: "Invalid branch naming ${config.branch}. NO NUGET PUSH, NO JIRA PUSH", buildResult: 'UNSTABLE', stageResult: 'UNSTABLE'){
+		error ("Invalid branch naming ${config.branch}. NO NUGET PUSH, NO JIRA PUSH")  }
+        }  
+	dir("${workspace}@tmp") {
+          deleteDir()
+        }
+	chuckNorris ()
+      }	
