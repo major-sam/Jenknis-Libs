c053601b49e8a562e4d247de8d7dad158bae5a5f
vrebyachih <vrebyachih@baltbet.ru>
2021-08-31 15:50:43 +0300

    count add

--- vars/checkoutBranch.groovy
+++ vars/checkoutBranch.groovy
@@ -1,14 +1,12 @@
 def call(Map config = [:]){
-  echo ("Это костыльный шаг - вложенный чекаут для выбранной ранее ветки. Следует пулить всю ветку с Jenkinsfile  и параметрзовать сборку от типа ветки")
-  echo "You choose ${config.BRANCH} branch, checkout"
-  BRANCH = config.BRANCH.trim().toString().replaceAll("\\s","")
-  env.DEFAULT_BRANCH = env.DEFAULT_BRANCH.trim().toString().replaceAll("\\s","")
+  echo ("Это костыльный шаг - вложенный чекаут для выбранной ранее ветки. Следует пулить всю ветку с Jenkinsfile  ")
+  echo "You choose ${config.branch} branch, checkout"
   withCredentials([gitUsernamePassword(credentialsId: config.creds)]) {
-  	if (BRANCH.contains (env.DEFAULT_BRANCH)) {
-    	  powershell ( encoding:"UTF8", script: "git clone ${config.GIT_URL} ${config.CLONE_FOLDER}")
+  	if (config.branch.contains (config.defaultBranch)) {
+          powershell ( encoding:"UTF8", script: "git clone ${config.gitUrl} ${config.cloneFolder}")
   	}
   	else {
-  	  powershell ( encoding:"UTF8", script: "git clone --single-branch --branch ${config.BRANCH} ${config.GIT_URL} ${config.CLONE_FOLDER}")
+          powershell ( encoding:"UTF8", script: "git clone --single-branch --branch ${config.branch} ${config.gitUrl} ${config.cloneFolder}")
   	}
   }
 }
