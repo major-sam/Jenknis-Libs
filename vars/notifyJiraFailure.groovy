164a0815bf01efa764b320e7b19a9d5f2e34f626
vrebyachih <vrebyachih@baltbet.ru>
2021-10-01 18:05:51 +0300

    Edit sucseess jira link commit

--- vars/notifyJiraFailure.groovy
+++ vars/notifyJiraFailure.groovy
@@ -1,8 +1,8 @@
 def call(Map config  = [:]){
-	jiraComment issueKey:config.issueKey ,body: """(x)
+	jiraAddComment idOrKey:config.issueKey ,input : [ body: """(x)
 h1. {color:#FF0000}BUILD ${config.buildName} FAILURE{color}
 h2. Jenkins build ${env.BUILD_NUMBER}
 [link |${env.BUILD_url}]
 ----
-"""    
+"""    ]
 }
--- vars/notifyJiraSuccses.groovy
+++ vars/notifyJiraSuccses.groovy
@@ -1,16 +1,17 @@
 def call(Map config = [:]){
-        if (config.release){
-	    nugetVersion = (env.build_number)
-        }else{
-	    nugetVersion = (env.build_number + "-" + config.branch).replace("/","-")
-	}
-	url = env.nexusBrowse + env.nexusNugetHosted + config.buildname + '%2F' +  "1.0.${nugetVersion}"
-	jiraComment issueKey: config.issueKey ,body: """(/)
+        commentBody = """(/)
 h1. {color:#00875A}BUILD ${config.buildName} SUCCSESFULL{color}
 h2. Jenkins build ${env.BUILD_NUMBER}
 [link |${env.BUILD_URL}]
-----
+"""
+	for (url in config.browseUrl){
+	   link = """----
 h2. Artifact link
-[link |${url}]
-"""  
+[link |${env.nexusBrowse}${url}]
+"""
+           commentBody = commentBody.concat(link)
+	   println link
+	}
+	println commentBody
+	jiraAddComment idOrKey: config.issueKey , input: [ body: commentBody]
 }
