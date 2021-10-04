b38126357b9a3ebae180f3f010ec59e431723f11
vrebyachih <vrebyachih@baltbet.ru>
2021-10-04 20:20:33 +0300

    add jira tasc to descr

--- vars/notifyJiraSuccses.groovy
+++ vars/notifyJiraSuccses.groovy
@@ -4,14 +4,19 @@ h1. {color:#00875A}BUILD ${config.buildName} SUCCSESFULL{color}
 h2. Jenkins build ${env.BUILD_NUMBER}
 [link |${env.BUILD_URL}]
 """
+	artifactUrl= 'Artifacts in Nexus Repository:<br>'
 	for (url in config.browseUrl){
 	   link = """----
 h2. Artifact link
 [link |${env.nexusBrowse}${url}]
 """
+	   	
+           currentBuild.description = currentBuild.description + "<a href=${env.nexusBrowse}${url}>Nexus Artifact</a> <br>"
+	   artifactUrl = artifactUrl + "<a href=${env.nexusBrowse}${url}>Nexus Artifact</a> <br>"
            commentBody = commentBody.concat(link)
-	   println link
+
 	}
-	println commentBody
 	jiraAddComment idOrKey: config.issueKey , input: [ body: commentBody]
+	writeFile file: 'link.html', text: artifactUrl
+	archiveArtifacts allowEmptyArchive: true, artifacts: 'link.html', caseSensitive: false, followSymlinks: false
 }
