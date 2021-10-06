e06318b9a42ec78d3c31d452334626943e4bf879
vrebyachih <vrebyachih@baltbet.ru>
2021-10-06 20:02:59 +0300

    fix jira comments

--- vars/notifyJiraFailure.groovy
+++ vars/notifyJiraFailure.groovy
@@ -1,8 +1,5 @@
 def call(Map config  = [:]){
-	jiraAddComment idOrKey:config.issueKey ,input : [ body: """(x)
-h1. {color:#FF0000}BUILD ${config.buildName} FAILURE{color}
-h2. Jenkins build ${env.BUILD_NUMBER}
-[link |${env.BUILD_url}]
-----
-"""    ]
+	jiraAddComment idOrKey:config.issueKey ,input : [ body: """{panel}
+	(x){color:#FF0000}BUILD *${config.buildName}* failed{color}
+{panel}"""    ]
 }
--- vars/notifyJiraSuccses.groovy
+++ vars/notifyJiraSuccses.groovy
@@ -1,19 +1,16 @@
 def call(Map config = [:]){
-        commentBody = """(/)
-h1. {color:#00875A}BUILD ${config.buildName} SUCCSESFULL{color}
-h2. Jenkins build ${env.BUILD_NUMBER}
-[link |${env.BUILD_URL}]
-"""
+        commentBody = """{panel}
+(/) {color:#00875A}+_Build *${config.buildName}* completed successfully_+{color}
+----"""
 	artifactUrl= 'Artifacts in Nexus Repository:<br>'
 	for (url in config.browseUrl){
-	   link = """----
-h2. Artifact link
-[link |${env.nexusBrowse}${url}]
+	   link = """
+[Artifact link |${env.nexusBrowse}${url}]
 """
 	   	
-           currentBuild.description = currentBuild.description + "<a href=${env.nexusBrowse}${url}>Nexus Artifact</a> <br>"
-	   artifactUrl = artifactUrl + "<a href=${env.nexusBrowse}${url}>Nexus Artifact</a> <br>"
-           commentBody = commentBody.concat(link)
+           currentBuild.description = currentBuild.description + "<a href=${env.nexusBrowse}${url}>Nexus Artifact </a> <br>"
+	   artifactUrl = artifactUrl + "<a href=${env.nexusBrowse}${url}>Nexus Artifact </a> <br>"
+           commentBody = commentBody.concat(link).concat("{panel}")
 
 	}
 	jiraAddComment idOrKey: config.issueKey , input: [ body: commentBody]
